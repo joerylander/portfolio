41ea364131b45f3cc341343f75aafcfaa07ea93c
Joe Rylander <rylander.j92@gmail.com>
2022-11-01 09:00:11 +0100

    initial header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 type Props = {}
 
 export default function Header({}: Props) {
@@ -7,7 +8,21 @@ export default function Header({}: Props) {
     <header 
         className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'
     >
-        <div className='flex flex-row items-center'>
+        <motion.div 
+        initial={{
+							x: -500,
+							opacity: 0,
+							scale: 0.5
+            }}
+        animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1
+        }}
+				transition={{
+					duration: 1.5
+				}}
+        className='flex flex-row items-center'>
             {/* Social Icons */}
             <SocialIcon 
                 url='https://twitter.com/CyberJojo9' 
@@ -24,8 +39,23 @@ export default function Header({}: Props) {
                 fgColor='gray'
                 bgColor='transparent'
             />
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+					initial={{
+						x: 500,
+						opacity: 0,
+						scale: 0.5
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1
+					}}
+					transition={{
+						duration: 1.5
+					}}
+					className='flex flex-row items-center text-gray-300 cursor-pointer'
+				>
             <SocialIcon
                 className='cursor-pointer'
                 network='email'
@@ -35,7 +65,7 @@ export default function Header({}: Props) {
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
                 Get in touch
             </p>
-        </div>
+        </motion.div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ export default function Home() {
         <title>JoJo's Portfolio</title>
       </Head>
       
-      {/* Header */}
       <Header />
       {/* Hero */}
 
