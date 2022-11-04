2db0d86bce5d3880d57ee59deb3e51940bc38876
Joe Rylander <rylander.j92@gmail.com>
2022-11-04 15:40:54 +0100

    type definitions

---
+++ sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
+
+export const config: any = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-10-21', // Learn more: https://www.sanity.io/docs/api-versioning
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source: any) => {
+  return createImageUrlBuilder(config).image(source)
+}
