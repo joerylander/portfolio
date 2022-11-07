e983c7da26ee9b5855edada8b42c95befac82463
Joe Rylander <rylander.j92@gmail.com>
2022-11-07 16:10:02 +0100

    pageinfo type bugfix

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,7 +3,7 @@ import { PageInfo } from "../typings"
 export const fetchPageInfo = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
   const data = await res.json()
-  const pageInfo: PageInfo[] = data.pageInfo
+  const pageInfo: PageInfo = data.pageInfo
   console.log('fetching pageInfo', pageInfo);
 
   return pageInfo
