e82f621d2355f22b7caed1920146d519afe0f0ed
Joe Rylander <rylander.j92@gmail.com>
2022-11-19 10:00:24 +0100

    refactored contact form post to more secure

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-function calculate(operation) {
-  switch (operation) {
-    case "ADD":
-      return function (a, b) {
-        console.log(`${a} + ${b} = ${a + b}`);
-      };
-    case "SUBTRACT":
-      return function (a, b) {
-        console.log(`${a} - ${b} = ${a - b}`);
-      };
-  }
-}
+// function calculate(operation) {
+//   switch (operation) {
+//     case "ADD":
+//       return function (a, b) {
+//         console.log(`${a} + ${b} = ${a + b}`);
+//       };
+//     case "SUBTRACT":
+//       return function (a, b) {
+//         console.log(`${a} - ${b} = ${a - b}`);
+//       };
+//   }
+// }
 
-const add = calculate("ADD")(5, 6);
-const subtract = calculate("SUBTRACT");
-// console.log(add(1, 2));
+// const add = calculate("ADD")(5, 6);
+// const subtract = calculate("SUBTRACT");
+// // console.log(add(1, 2));
 
-function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `grimey rgba(var(${variableName}), ${opacityValue})`;
-    }
-    console.log('hej', opacityValue);
-    return `rgb(var(${variableName}))`;
-  };
-}
+// function withOpacity(variableName) {
+//   return ({ opacityValue }) => {
+//     if (opacityValue !== undefined) {
+//       return `grimey rgba(var(${variableName}), ${opacityValue})`;
+//     }
+//     console.log('hej', opacityValue);
+//     return `rgb(var(${variableName}))`;
+//   };
+// }
 
-const color = withOpacity("--color-text-base")({ opacityValue: 0.5 });
-console.log(color);
+// const color = withOpacity("--color-text-base")({ opacityValue: 0.5 });
+// console.log(color);
