75e20596be55521ad8b5ab58510282ee92f6b9f6
Joe Rylander <rylander.j92@gmail.com>
2022-11-27 10:31:55 +0100

    skills responsive modified

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -13,17 +13,9 @@ const Skill = ({ skill, directionLeft }: Props) => {
   return (
     <div className='group relative flex cursor-pointer'>
       <motion.div
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0
-        }}
-        transition={{
-          duration: 1
-        }}
-        whileInView={{
-          opacity: 1,
-          x: 0
-        }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
       >
         <Image
           loader={() => urlFor(skill?.image).url()}
@@ -31,16 +23,16 @@ const Skill = ({ skill, directionLeft }: Props) => {
           alt='skill'
           width={40}
           height={10}
-          className='rounded-full border border-th-border-base object-cover w-24 h-24
-        md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-out'
+          className='rounded-full border border-th-border-base object-cover w-14 h-14 sm:w-20 sm:h-20
+        md:w-24 md:h-24 xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-out'
         />
       </motion.div>
 
       <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
-       group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
+       group-hover:bg-white w-10 h-10 sm:h-24 sm:w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
       >
         <div className='flex items-center justify-center h-full'>
-          <p className='text-3xl font-bold text-black opacity-100'>{skill?.progress}%</p>
+          <p className='text-2xl sm:text-3xl font-bold text-black opacity-100'>{skill?.progress}%</p>
         </div>
       </div>
     </div>
