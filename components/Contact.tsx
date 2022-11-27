c48ee461f014e6e80671d7c502d864b04728996f
Joe Rylander <rylander.j92@gmail.com>
2022-11-27 13:28:16 +0100

    contact responsive mod

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -47,30 +47,30 @@ const Contact = () => {
       </h3>
 
       <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>
+        <h4 className='text-base sm:text-2xl md:text-4xl font-semibold text-center'>
           I have got just what you need.
           <span className='underline decoration-th-highlight-50'> Let&apos;s talk.</span>
         </h4>
 
         <div className='space-y-10'>
           <div className='flex items-center space-x-5'>
-            <PhoneIcon className='text-th-highlight h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>+46761842847</p>
+            <PhoneIcon className='text-th-highlight h-5 w-5 md:h-7 md:w-7 animate-pulse' />
+            <p className='text-sm sm:text-lg md:text-2xl'>+46761842847</p>
           </div>
 
           <div className='flex items-center space-x-5'>
-            <EnvelopeIcon className='text-th-highlight h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>rylander.j92@gmail.com</p>
+            <EnvelopeIcon className='text-th-highlight h-5 w-5 md:h-7 md:w-7 animate-pulse' />
+            <p className='text-sm sm:text-lg md:text-2xl'>rylander.j92@gmail.com</p>
           </div>
 
           <div className='flex items-center space-x-5'>
-            <MapPinIcon className='text-th-highlight h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>Lysviksgatan 40 | 123 42 | Stockholm, Sweden</p>
+            <MapPinIcon className='text-th-highlight h-5 w-5 md:h-7 md:w-7 animate-pulse' />
+            <p className='text-sm sm:text-lg md:text-2xl'>Lysviksgatan 40 | 123 42 | Stockholm, Sweden</p>
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
-          <div className='flex space-x-2'>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col flex-wrap space-y-2 w-fit mx-auto'>
+          <div className='flex flex-col sm:flex-row space-x-0 space-y-2 sm:space-x-3 sm:space-y-0'>
             <input {...register('name')} placeholder='Name' name='name' className='contactInput' type="text" required />
             <input {...register('email')} placeholder='Email' name='email' className='contactInput' type="email" required />
           </div>
