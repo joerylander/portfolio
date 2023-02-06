13106fb61d3c1dda2a5fb525b86b5fcdcc194671
Joe Rylander <rylander.j92@gmail.com>
2023-02-06 17:06:41 +0100

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,103 @@
-import type { GetStaticProps } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import WorkExperience from '../components/WorkExperience'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
-import { Experience, PageInfo, Project, Skill, Social, ContactType } from '../typings'
-import { fetchPageInfo } from '../utils/fetchPageInfo'
-import { fetchExperiences } from '../utils/fetchExperiences'
-import { fetchSkills } from '../utils/fetchSkills'
-import { fetchProjects } from '../utils/fetchProjects'
-import { fetchSocials } from '../utils/fetchSocials'
-import { urlFor } from '../sanity'
+import type { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSocials } from "../utils/fetchSocials";
+import { urlFor } from "../sanity";
+import favicon from "../public/favicon.ico";
 
 type Props = {
-	pageInfo: PageInfo
-	experiences: Experience[]
-	skills: Skill[]
-	projects: Project[]
-	socials: Social[]
-}
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
-	return (
-		<div
-			className='bg-th-bg-base text-white h-screen snap-y snap-mandatory
+  return (
+    <div
+      className="bg-th-bg-base text-white h-screen snap-y snap-mandatory
 				overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
-				scrollbar-thumb-[#F7AB0A]'
-		>
-			<Head>
-				<title>Portfolio - Joe Rylander</title>
-			</Head>
-			<Header socials={socials} />
+				scrollbar-thumb-[#F7AB0A]"
+    >
+      <Head>
+        <link rel="icon" href={favicon.src} />
+        <title>Portfolio - Joe Rylander</title>
+      </Head>
+      <Header socials={socials} />
 
-			<section id='hero' className='snap-start'>
-				<Hero pageInfo={pageInfo} />
-			</section>
+      <section id="hero" className="snap-start">
+        <Hero pageInfo={pageInfo} />
+      </section>
 
-			<section id='about' className='snap-center'>
-				<About pageInfo={pageInfo} />
-			</section>
+      <section id="about" className="snap-center">
+        <About pageInfo={pageInfo} />
+      </section>
 
-			<section id='experience' className='snap-center'>
-				<WorkExperience experiences={experiences} />
-			</section>
+      <section id="experience" className="snap-center">
+        <WorkExperience experiences={experiences} />
+      </section>
 
-			<section id='skills' className='snap-start'>
-				<Skills skills={skills} />
-			</section>
+      <section id="skills" className="snap-start">
+        <Skills skills={skills} />
+      </section>
 
-			<section id='projects' className='snap-start'>
-				<Projects projects={projects} />
-			</section>
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
 
-			<section id='contact' className='snap-start'>
-				<Contact />
-			</section>
+      <section id="contact" className="snap-start">
+        <Contact />
+      </section>
 
-			<Link href='#hero'>
-				<footer className='sticky bottom-5 w-full cursor-pointer'>
-					<div className='flex items-center justify-center'>
-						<Image
-							className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
-							loader={() => urlFor(pageInfo?.heroImage).url()}
-							src={urlFor(pageInfo.heroImage).url()}
-							alt='hero'
-							width={40}
-							height={40}
-						/>
-					</div>
-				</footer>
-			</Link>
-		</div>
-	)
-}
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              loader={() => urlFor(pageInfo?.heroImage).url()}
+              src={urlFor(pageInfo.heroImage).url()}
+              alt="hero"
+              width={40}
+              height={40}
+            />
+          </div>
+        </footer>
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-	const pageInfo: PageInfo = await fetchPageInfo()
-	const experiences: Experience[] = await fetchExperiences()
-	const skills: Skill[] = await fetchSkills()
-	const projects: Project[] = await fetchProjects()
-	const socials: Social[] = await fetchSocials()
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
 
-	return {
-		props: {
-			pageInfo,
-			experiences,
-			skills,
-			projects,
-			socials
-		},
-		revalidate: 120,
-	}
-}
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 120,
+  };
+};
