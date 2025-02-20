d246e54ad5206e25c17aa83f0f113fb6e0faf5cb
Joe Rylander <rylander.media@gmail.com>
2025-02-20 17:53:57 +0100

    alter copy

--- client/src/app/(app)/Header.js
+++ client/src/app/(app)/Header.js
@@ -2,7 +2,6 @@ const Header = ({ title }) => {
     return (
         <header className="bg-white shadow">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
                     {title}
                 </h2>
