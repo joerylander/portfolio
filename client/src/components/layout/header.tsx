991e0736bb1a6aa7f69ce3d2514fe453c70e50b9
Joe Rylander <rylander.j92@gmail.com>
2025-06-08 09:18:02 +0200

    refactor: remove unused imports and clean up Logo component

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import { azaret_mono, inter } from '@/lib/fonts';
+import { azaret_mono } from '@/lib/fonts';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import Logo from '@/components/shared/logo';
@@ -6,9 +6,7 @@ import Logo from '@/components/shared/logo';
 export default function Header() {
   return (
     <header className="mx-6 flex h-42 items-center justify-between">
-      {/* Logo */}
       <Logo />
-      {/* Navbar */}
       <nav className="space-x-24">
         <Link href="#services" className={azaret_mono.className}>
           Service
