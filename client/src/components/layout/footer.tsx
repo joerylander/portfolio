c0797e68b194c3949227bb1966c4bf2cc281beca
Joe Rylander <rylander.j92@gmail.com>
2025-06-16 08:47:15 +0200

    feat: update Footer component layout and content; enhance structure and add contact information

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -2,8 +2,23 @@ import { azaret_mono } from '@/lib/fonts';
 
 export default function Footer() {
   return (
-    <footer className="mx-6 flex items-center justify-center">
-      <h3 className={azaret_mono.className}>Footer from component</h3>
+    <footer className="mx-6 flex min-h-20 items-center justify-between">
+      <p className={`${azaret_mono.className} capitalize`}>
+        &#169; rylander media 2025. all rights reserved
+      </p>
+
+      <div className="flex">
+        <div className="">Github</div>
+        <div className="">Linkedin</div>
+        <div className="">Instagram</div>
+      </div>
+
+      <a
+        href="mailto:rylander.media@gmail.com"
+        className={`${azaret_mono.className}`}
+      >
+        rylander.media@gmail.com
+      </a>
     </footer>
   );
 }
