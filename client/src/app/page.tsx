e74f774a20d42ec7d46f91cf3d009c4b23a410a8
Joe Rylander <rylander.j92@gmail.com>
2025-07-03 16:49:27 +0800

    feat: implement smooth scrolling for hash links on the Home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,24 @@
+'use client';
+
 import SectionWrapper from '@/components/layout/sectionWrapper';
 import Hero from '@/components/page/hero';
 import Services from '@/components/page/services';
 import Projects from '@/components/page/projects';
 import Testimonials from '@/components/page/testimonials';
 import Contact from '@/components/page/contact';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
+
   return (
     <>
       <SectionWrapper id="hero" className="snap-center" animate={true}>
