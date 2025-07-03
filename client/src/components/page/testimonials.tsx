9b44d79d3a8e3b59e585accee65c1a78f6ce88c1
Joe Rylander <rylander.j92@gmail.com>
2025-07-03 17:21:01 +0800

    feat: add testimonials data and update Testimonials component to display them

--- client/src/components/page/testimonials.tsx
+++ client/src/components/page/testimonials.tsx
@@ -2,6 +2,7 @@
 
 import { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
+import { testimonials } from '@/db/data.json';
 import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { azaret_mono, inter } from '@/lib/fonts';
@@ -14,32 +15,35 @@ export default function Testimonials() {
     <Carousel
       plugins={[autoplay.current]}
       opts={{ loop: true, align: 'center' }}
-      className="w-full max-w-lg"
+      className="w-full max-w-xl"
       onMouseEnter={autoplay.current.stop}
       onMouseLeave={autoplay.current.reset}
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, i) => (
-          <CarouselItem key={i}>
+        {testimonials.map((t) => (
+          <CarouselItem key={t.id}>
             <article className="flex h-[500px] max-w-fit">
               <div className="flex flex-col items-center justify-evenly">
-                <Avatar className="h-48 w-48">
-                  <AvatarImage src="/images/profile.jpg" alt="avatar" />
-                  <AvatarFallback>Initials</AvatarFallback>
+                <Avatar className="h-40 w-40">
+                  <AvatarImage
+                    src={`/images/${t.img_src}`}
+                    alt={`avatar of ${t.name.toLowerCase()}`}
+                  />
+                  <AvatarFallback>{t.name.slice(0, 2)}</AvatarFallback>
                 </Avatar>
 
                 <p className={`${azaret_mono.className} text-center`}>
-                  From day one, the execution was tight and timelines were
-                  respected. Our site looks and feels premium, and feedback has
-                  been all fire.
+                  {t.testimonial}
                 </p>
-                <h3 className={`${inter.className} text-4xl font-bold`}>
-                  Morgan Freeman {i}
+                <h3
+                  className={`${inter.className} text-4xl font-bold capitalize`}
+                >
+                  {t.name}
                 </h3>
                 <h4
-                  className={`${azaret_mono.className} font-semibold opacity-80`}
+                  className={`${azaret_mono.className} font-semibold uppercase opacity-80`}
                 >
-                  Product Manager
+                  {t.title}
                 </h4>
               </div>
             </article>
