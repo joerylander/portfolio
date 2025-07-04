8889a01a85d9550bcb7034e9a81b1eb05024e11c
Joe Rylander <rylander.j92@gmail.com>
2025-07-04 14:19:13 +0800

    refactor: remove commented-out input fields in Contact component

--- client/src/components/page/contact.tsx
+++ client/src/components/page/contact.tsx
@@ -7,10 +7,8 @@ import { Button } from '../ui/button';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from '../ui/form';
 import { Input } from '../ui/input';
@@ -128,31 +126,6 @@ export default function Contact() {
               </FormItem>
             )}
           />
-          {/* <div className="flex gap-10">
-            <Input
-              id="firstName"
-              type="text"
-              placeholder="First Name"
-              className={azaret_mono.className}
-            />
-            <Input
-              id="lastName"
-              type="text"
-              placeholder="Last Name"
-              className={azaret_mono.className}
-            />
-          </div>
-          <Input
-            id="email"
-            type="text"
-            placeholder="E-mail"
-            className={azaret_mono.className}
-          />
-          <Textarea
-            id="message"
-            placeholder="Describe what you need help with..."
-            className={`${azaret_mono.className} h-32`}
-          />*/}
           <Button type="submit" size="lg" className="mx-auto h-12 w-1/2">
             Let's get started!
           </Button>
