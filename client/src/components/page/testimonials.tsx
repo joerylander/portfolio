adada8c6f2246ea934593d427382262110300aaf
Joe Rylander <rylander.j92@gmail.com>
2025-07-07 12:49:55 +0800

    Refactor data to add real testimonials over placeholders

--- client/src/components/page/testimonials.tsx
+++ client/src/components/page/testimonials.tsx
@@ -35,35 +35,40 @@ export default function Testimonials() {
     >
       <CarouselContent>
         {data &&
-          data.map((t) => (
-            <CarouselItem key={t.id}>
-              <article className="flex h-[500px] max-w-fit">
-                <div className="flex flex-col items-center justify-evenly">
-                  <Avatar className="h-40 w-40">
-                    <AvatarImage
-                      src={`/images/${t.img_src}`}
-                      alt={`avatar of ${t.name.toLowerCase()}`}
-                    />
-                    <AvatarFallback>{t.name.slice(0, 2)}</AvatarFallback>
-                  </Avatar>
+          data.map((t) => {
+            const nameParts = t.name.split(' ');
+            const initials = nameParts[0][0] + nameParts[1][0];
+            return (
+              <CarouselItem key={t.id}>
+                <article className="flex h-[500px] max-w-fit">
+                  <div className="flex flex-col items-center justify-evenly">
+                    <Avatar className="h-40 w-40">
+                      <AvatarImage
+                        src={`/images/testimonials/${t.img_src}`}
+                        alt={`avatar of ${t.name.toLowerCase()}`}
+                        className="object-cover"
+                      />
+                      <AvatarFallback>{initials}</AvatarFallback>
+                    </Avatar>
 
-                  <p className={`${azaret_mono.className} text-center`}>
-                    {t.testimonial}
-                  </p>
-                  <h3
-                    className={`${inter.className} text-4xl font-bold capitalize`}
-                  >
-                    {t.name}
-                  </h3>
-                  <h4
-                    className={`${azaret_mono.className} font-semibold uppercase opacity-80`}
-                  >
-                    {t.title}
-                  </h4>
-                </div>
-              </article>
-            </CarouselItem>
-          ))}
+                    <p className={`${azaret_mono.className} text-center`}>
+                      {t.testimonial}
+                    </p>
+                    <h3
+                      className={`${inter.className} text-4xl font-bold capitalize`}
+                    >
+                      {t.name}
+                    </h3>
+                    <h4
+                      className={`${azaret_mono.className} font-semibold uppercase opacity-80`}
+                    >
+                      {t.title}
+                    </h4>
+                  </div>
+                </article>
+              </CarouselItem>
+            );
+          })}
       </CarouselContent>
     </Carousel>
   );
