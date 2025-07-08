0b215e0d74f1ee5d45a3e67970d86c8a279ba17c
Joe Rylander <rylander.j92@gmail.com>
2025-07-08 16:59:46 +0800

    feat: add skeleton loading for services to enhance user experience during data fetching

--- client/src/components/page/services.tsx
+++ client/src/components/page/services.tsx
@@ -2,10 +2,10 @@ import { azaret_mono, inter } from '@/lib/fonts';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { useFetch } from '@/lib/fetch';
 import { Service } from '@/types/types';
+import { Skeleton } from '../ui/skeleton';
 
 export default function Services() {
   const { data, loading } = useFetch<Service[]>('/api/services');
-  if (loading) return <div>Loading services...</div>;
   return (
     <>
       <h2
@@ -14,27 +14,46 @@ export default function Services() {
         Your Website Should Do more Than Just Look Good - It Should Convert
       </h2>
       <div className="flex flex-col gap-10 lg:flex-row">
-        {!loading &&
-          data &&
-          data.map((service) => (
-            <Card
-              key={service.id}
-              className="flex w-full max-w-sm bg-[#262626] text-center capitalize"
-            >
-              <CardHeader>
-                <CardTitle>
-                  <h3 className="text-base font-bold sm:text-lg md:text-xl">
-                    {service.title}
-                  </h3>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className={`${azaret_mono.className} font-light`}>
-                  {service.description}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
+        {!loading
+          ? Array.from({ length: 3 }).map((_, index) => (
+              <Card
+                key={`skeleton-${index}`}
+                className="flex min-h-[225px] w-full max-w-sm min-w-[350px] bg-[#262626] text-center capitalize"
+              >
+                <CardHeader>
+                  <CardTitle>
+                    <Skeleton className="mx-auto h-6 w-3/4" />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-col items-center gap-4">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-7/8" />
+                    <Skeleton className="h-4 w-4/5" />
+                  </div>
+                </CardContent>
+              </Card>
+            ))
+          : data &&
+            data.map((service) => (
+              <Card
+                key={service.id}
+                className="flex w-full max-w-sm bg-[#262626] text-center capitalize"
+              >
+                <CardHeader>
+                  <CardTitle>
+                    <h3 className="text-base font-bold sm:text-lg md:text-xl">
+                      {service.title}
+                    </h3>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className={`${azaret_mono.className} font-light`}>
+                    {service.description}
+                  </p>
+                </CardContent>
+              </Card>
+            ))}
       </div>
     </>
   );
